1326a75a1dec3dcefade5eb0d2faeabfb6583d3e
Kevin Harris <kevin.harris@diaceutics.com>
2025-05-05 09:47:09 +0100

    Day 4 submission

--- Day 4/Rock Paper Scissors/task.py
+++ Day 4/Rock Paper Scissors/task.py
@@ -29,11 +29,16 @@ scissors = '''
 computer_choice = random.randint(0, 2)
 my_choice = input('1 for rock, 2 for paper, 3 for scissors: ')
 
+if not my_choice in ['1', '2', '3']:
+    print('Invalid choice')
+    exit()
+
 options = [rock, paper, scissors]
 
 int_choice = int(my_choice) - 1
 print(options[int_choice])
 print(options[computer_choice])
+
 outcome = 'You win'
 if computer_choice == int_choice:
     outcome = 'Draw'
